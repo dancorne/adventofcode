362685d6e254794d03117e234aec756ccf4877ed
Dan Corne <dan@corne.co.uk>
2023-12-10 07:23:13 +0000

    Add very inefficient solution to 2023 day 5
    Took 4.5 hours to find the solution! Definitely could do some more
    preprocessing to avoid doing things in the main loop, but probably using
    the wrong approach...

--- 2023/day5.rb
+++ 2023/day5.rb
@@ -46,10 +46,43 @@ class Day5
     end
     lowest
   end
+
+  def reverse_mapped_number(mapping, destination)
+    @mappings[mapping].each do |line|
+      destination_start, source_start, len = line.split.map(&:to_i)
+      if (destination_start..destination_start + len).include?(destination)
+        return (source_start - destination_start + destination)
+      end
+    end
+    destination
+  end
+
+  def in_seed_ranges?(seed)
+    ranges = []
+    @seeds.each_slice(2) do |start, len|
+      ranges << (start..start + len)
+    end
+    ranges.any? { |range| range.include?(seed) }
+  end
+
+  def part2
+    (0..).each do |location| # Totally safe
+      humidity = reverse_mapped_number('humidity-to-location', location)
+      temperature = reverse_mapped_number('temperature-to-humidity', humidity)
+      light = reverse_mapped_number('light-to-temperature', temperature)
+      water = reverse_mapped_number('water-to-light', light)
+      fertilizer = reverse_mapped_number('fertilizer-to-water', water)
+      soil = reverse_mapped_number('soil-to-fertilizer', fertilizer)
+      seed = reverse_mapped_number('seed-to-soil', soil)
+      p "#{location}: #{seed}"
+      return location if in_seed_ranges?(seed)
+    end
+    raise 'Bug: nothing found'
+  end
 end
 
 if __FILE__ == $PROGRAM_NAME
   input = File.read('./input/day5')
   puts Day5.new(input).part1
-  # puts Day5.new(input).part2
+  puts Day5.new(input).part2
 end
