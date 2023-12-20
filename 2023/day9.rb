cdb15a72be48fd5ba3bcbeee2cb70a8f2e7ce6fc
Dan Corne <dan@corne.co.uk>
2023-12-20 22:06:32 +0000

    Add solution to 2023 day 9 part 2
    My solution for day 8 part 2 is still running while I finished this
    one...!

--- 2023/day9.rb
+++ 2023/day9.rb
@@ -5,20 +5,31 @@ class Day9
     @histories = input.lines.map { |line| line.split.map(&:to_i) }
   end
 
-  def recurse(arr)
+  def calculate_next(arr)
     return arr[0] if arr.uniq.length == 1
 
     new_array = (1...arr.length).map { |n| arr[n] - arr[n - 1] }
-    arr[-1] + recurse(new_array)
+    arr[-1] + calculate_next(new_array)
   end
 
   def part1
-    @histories.sum { |h| recurse(h) }
+    @histories.sum { |h| calculate_next(h) }
+  end
+
+  def calculate_previous(arr)
+    return arr[0] if arr.uniq.length == 1
+
+    new_array = (1...arr.length).map { |n| arr[n] - arr[n - 1] }
+    arr[0] - calculate_previous(new_array)
+  end
+
+  def part2
+    @histories.sum { |h| calculate_previous(h) }
   end
 end
 
 if __FILE__ == $PROGRAM_NAME
   input = File.read('./input/day9')
   puts Day9.new(input).part1
-  # puts Day9.new(input).part2
+  puts Day9.new(input).part2
 end
--- 2023/day9_test.rb
+++ 2023/day9_test.rb
@@ -10,4 +10,13 @@ class Day9Test < Minitest::Test
     INPUT
     assert_equal 114, Day9.new(input).part1
   end
+
+  def test_part2
+    input = <<~INPUT
+      0 3 6 9 12 15
+      1 3 6 10 15 21
+      10 13 16 21 30 45
+    INPUT
+    assert_equal 2, Day9.new(input).part2
+  end
 end
